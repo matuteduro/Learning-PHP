eb7ccfdf7be6aac754e339f4aa3f854591a028a4
matuteduro <matias_duro@hotmail.com>
2022-05-23 22:06:15 -0300

    ejercicio 3 completado

---
+++ ejercicio3.php
@@ -0,0 +1,16 @@
+
+
+<?php
+if($_GET){
+
+    $nombre=$_GET['nombre'];
+
+    echo "Hola ".$nombre;
+}
+
+/*
+Metodo GET muestra los datos en la URL
+Metodo Post oculta los datos en la URL
+*/
+
+?>
