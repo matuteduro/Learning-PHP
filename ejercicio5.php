686c4007453306df3cb78109c259973883b28b7b
matuteduro <matias_duro@hotmail.com>
2022-05-23 22:20:38 -0300

    ejercicio 5 completado

---
+++ ejercicio5.php
@@ -0,0 +1,15 @@
+<?php
+
+$Edad=25; // Entero
+
+$Nombre="Matias Duro"; // String
+
+$Altura=1.4; // Decimal
+
+$Programador=null; // Valor nulo
+
+echo " El nombre es ".$Nombre." La edad es ".$Edad." su altura es ".$Altura." el puesto ".$Programador;
+
+
+
+?>
