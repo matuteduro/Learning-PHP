e048e810facfb37818cda8a08c398e7f7b624617
matuteduro <matias_duro@hotmail.com>
2022-05-23 22:27:23 -0300

    ejercicio 6 completado

---
+++ ejercicio6.php
@@ -0,0 +1,23 @@
+<?php
+
+$edad=25; // variables
+
+echo $edad."<br/>";
+
+$edad=30; // Cambiar valor de la variable
+
+echo $edad."<br/>";
+
+// Uso de constante
+
+define("NOMBRE","MATIAS");
+
+// Imprimir constante
+
+echo NOMBRE;
+
+// NOMBRE="Pedro"; // No me va a dejar porque la constante no lo permite
+
+// echo NOMBRE;
+
+?>
