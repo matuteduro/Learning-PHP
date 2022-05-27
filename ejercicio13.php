ccea69bd1b3319f682d323c084ac46eee91374a5
matuteduro <matias_duro@hotmail.com>
2022-05-27 02:35:06 -0300

    ejercicio 13 completado

---
+++ ejercicio13.php
@@ -0,0 +1,12 @@
+<?php
+
+//Ciclo For
+
+for ($numeroInicial=0; $numeroInicial<=10; $numeroInicial++){
+    echo "Numero ".$numeroInicial."<br/>";
+    
+}
+    if($numeroInicial>=10)
+    echo "El numero ya llego a su limite"
+
+?>
