cbc59cfe462a4995a1ee0581fca29339de5f269b
matuteduro <matias_duro@hotmail.com>
2022-05-27 21:21:08 -0300

    Ejercicio 28 + sql completado

--- ejercicio28.php
+++ ejercicio28.php
@@ -1,5 +1,6 @@
 <?php
 
+//http://localhost/phpmyadmin/
 
 $servidor="localhost"; // Es lo mismo que poner 127.0.0.1
 $user="root";
@@ -9,6 +10,11 @@ try{ // Si no sucede un error aqui
 
     $conexion=new PDO("mysql:host=$servidor;dbname=album", $user,$contrasenia );
     $conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    $sql="INSERT INTO `fotos` (`id`, `nombre`, `ruta`) VALUES (NULL, 'Jugando con la programacion', 'foto.jpg');";
+    // Armado en phpmyadmin
+    $conexion->exec($sql);
+
     echo "Conexion Establecida";
 
 }catch(PDOException $error){ // continuar con esto, sino el error lo abosrbera el catch
