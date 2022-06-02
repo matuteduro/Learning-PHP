9b1cd5f2e2e93e2f75b0fd747f7d23cb20fbf169
matuteduro <matias_duro@hotmail.com>
2022-06-02 16:31:10 -0300

    Ejercicio final 1

---
+++ portfolio/index.php
@@ -0,0 +1,13 @@
+<?php include("header.php"); ?>
+    
+
+<div class="p-5 bg-light">
+    <div class="container">
+        <h1 class="display-3">Bienvenidos</h1>
+        <p class="lead">Este es un portfolio privado</p>
+        <hr class="my-2">
+        <p>Mas informacion</p>
+    </div>
+</div> Hello Matias, I'm your home 
+
+    <?php include("footer.php"); ?>
---
+++ portfolio/portfolio.php
@@ -0,0 +1,4 @@
+<?php include("header.php"); ?>
+    Hello Matias, I'm your portfolio
+
+    <?php include("footer.php"); ?>
